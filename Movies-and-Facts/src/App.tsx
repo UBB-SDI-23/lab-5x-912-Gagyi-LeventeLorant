bd9ffcedb2c7141a0b81ad0a9cef520fdbd28f16
Gagyi Lori <gagyi.lorant@gmail.com>
2023-04-03 18:51:27 +0300

    Update2

--- Movies-and-Facts/src/App.tsx
+++ Movies-and-Facts/src/App.tsx
@@ -13,6 +13,7 @@ import { AllFilms } from "./components/films/AllFilms";
 import { FilmDetails } from "./components/films/FilmDetails";
 import { FilmDelete } from "./components/films/FilmDelete";
 import { FilmAdd } from "./components/films/FilmAdd";
+import { FilmUpdate } from "./components/films/FilmEdit"
 
 function App() {
 	return (
@@ -24,7 +25,7 @@ function App() {
 					<Route path="/" element={<AppHome />} />
 					<Route path="/films" element={<AllFilms />} />
 					<Route path="/films/:filmId/details" element={<FilmDetails />} />
-					<Route path="/films/:filmId/edit" element={<FilmDetails />} />
+					<Route path="/films/:filmId/edit" element={<FilmUpdate />} />
 					<Route path="/films/:filmId/delete" element={<FilmDelete />} />
 					<Route path="/films/add" element={<FilmAdd />} />
 				</Routes>
