13efb3419ec6b8e996d6049938241dff5531c1e9
Gagyi Lori <gagyi.lorant@gmail.com>
2023-04-03 19:30:35 +0300

    1

--- Movies-and-Facts/src/components/statistics/FilmActorPayStatistics.tsx
+++ Movies-and-Facts/src/components/statistics/FilmActorPayStatistics.tsx
@@ -26,7 +26,7 @@ export const FilmActorPayStatistics = () => {
 
 	useEffect(() => {
 		setLoading(true);
-		fetch(`${BACKEND_API_URL}/films/films_pay/`)
+		fetch(`${BACKEND_API_URL}/films/films_pay`)
 			.then((response) => response.json())
 			.then((data) => {
 				setFilms(data);
