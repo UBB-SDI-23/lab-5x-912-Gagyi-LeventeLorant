19e44ad4a14268c1ad542fe1e58f2095b62fc6fc
Gagyi Lori <gagyi.lorant@gmail.com>
2023-04-09 14:08:17 +0300

    [2] update populate code

--- MovieSite/mysite/populate_with_fakes.py
+++ MovieSite/mysite/populate_with_fakes.py
@@ -15,10 +15,8 @@ if __name__ == '__main__':
 
     fake = Faker()
 
-    with open('data.sql', 'w') as f:
+    n = 10000
+    for _ in range(n):
 
-        n = 10000
-        for _ in range(n):
-            
-            Film.objects.create(name=fake.name(), release_date=fake.date(), on_netfilx=fake.pybool(), profit=fake.random_int(1000000, 10000000), rating=fake.random_float(1,10))
-            Screening.objects.create(room=fake.name(), date=fake.date(),imax=fake.pybool(), tickets_bought=fake.random_int(1, 100), price= fake.random_float(5,25), film=Film.objects.last())
+        Film.objects.create(name=fake.name(), release_date=fake.date(), on_netfilx=fake.pybool(), profit=fake.random_int(1000000, 10000000), rating=fake.random_float(1,10))
+        Screening.objects.create(room=fake.name(), date=fake.date(),imax=fake.pybool(), tickets_bought=fake.random_int(1, 100), price= fake.random_float(5,25), film=Film.objects.last())
